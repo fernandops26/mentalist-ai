1dd5fa6c8a3f5495b5e84ea1ab51c39388b91932
Fernando <jf.palacios.sz@gmail.com>
2023-02-17 03:43:35 -0500

    head for map

---
+++ app/(app)/map/head.tsx
@@ -0,0 +1,14 @@
+export default function Head() {
+  return (
+    <>
+      <title>Your Content Ideation Map | Mentalist</title>
+      <meta content='width=device-width, initial-scale=1' name='viewport' />
+      <meta
+        name='description'
+        content='Easily Create Mind Maps and Generate Content and Title Ideas Based
+            on Topics'
+      />
+      <link rel='icon' href='/favicon.ico' />
+    </>
+  );
+}
