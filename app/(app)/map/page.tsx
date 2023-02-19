6c56ad2a14c8e919acf8337dbe24e4a669284b99
Fernando <jf.palacios.sz@gmail.com>
2023-02-19 00:18:35 -0500

    message for mobile views

--- app/(app)/map/page.tsx
+++ app/(app)/map/page.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link';
 export default async function MapPage() {
   return (
     <div className='flex w-full h-screen min-h-[calc(100vh_-_100px)]'>
-      <div className='w-52 p-4 bg-white h-full'>
+      <div className='hidden md:block w-52 p-4 bg-white h-full'>
         <Link href='/' className='flex items-center space-x-2'>
           <IconComponent name='logo' className='w-3 h-3' />
           <span className='inline-block font-bold'>Mentalist</span>
@@ -15,11 +15,33 @@ export default async function MapPage() {
           <Form />
         </div>
       </div>
-      <div className='flex-1'>
+      <div className='hidden md:block flex-1'>
         <div className='w-full bg-white h-full'>
           <div className='border h-full'>
             <Map />
           </div>
+        </div>
+      </div>
+      <div className=' md:hidden flex justify-center items-center w-full bg-slate-50'>
+        <div className='w-80 text-center mx-auto'>
+          <p className='font-bold text-xl text-slate-800'>Hi mentalister! 👋</p>
+          <p className=' text-slate-700 mt-4'>
+            We are working to expand the use of mentalist-ai on mobile view, for
+            now you can use it in browser
+          </p>
+          <p></p>
+          <p className='text-sm text-slate-400 mt-3'>
+            Follow on{' '}
+            <Link
+              href='https://twitter.com/fernandops26'
+              target='_blank'
+              rel='noreferrer'
+              className='underline underline-offset-4'
+            >
+              Twitter
+            </Link>{' '}
+            for updates
+          </p>
         </div>
       </div>
     </div>
